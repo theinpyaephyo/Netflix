07c1c7dc58e446aeb2250c797fa894bc750be3e4
Theinpyae Phyo <theinpyaephyo@gmail.com>
2022-05-12 11:41:41 -0400

    - scrollview

--- Netflix/ViewController/HomeViewController.swift
+++ Netflix/ViewController/HomeViewController.swift
@@ -20,6 +20,12 @@ class HomeViewController: UIViewController {
     
     @IBOutlet var tvHome: UITableView!
     
+    @IBOutlet var viewTop: UIView!
+    
+    @IBOutlet var viewTopConstraint: NSLayoutConstraint!
+    
+    var lastContentOffset: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -33,6 +39,7 @@ class HomeViewController: UIViewController {
         
         tvHome.register(UINib(nibName: ShowNewMovie.identifier, bundle: nil), forCellReuseIdentifier: ShowNewMovie.identifier)
         tvHome.dataSource = self
+        tvHome.delegate = self
         tvHome.separatorStyle = .none
         
        
@@ -54,7 +61,7 @@ class HomeViewController: UIViewController {
         svMovies.isHidden = true
     }
 }
-extension HomeViewController: UITableViewDataSource {
+extension HomeViewController: UITableViewDataSource , UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
@@ -62,5 +69,24 @@ extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ShowNewMovie.identifier, for: indexPath) as! ShowNewMovie
         return cell
+    }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y >= lastContentOffset {
+            UIView.animate(withDuration: 2) {
+//                self.viewTopConstraint.constant = -176.5
+            }
+
+            print("Scrolling up")
+        } else if scrollView.contentOffset.y <= lastContentOffset{
+            UIView.animate(withDuration: 2) {
+//                self.viewTopConstraint.constant = -46
+            }
+            print("Scrolling Down")
+        } else {
+            print("Not Scrolling")
+        }
+    }
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        lastContentOffset = scrollView.contentOffset.y
     }
 }
